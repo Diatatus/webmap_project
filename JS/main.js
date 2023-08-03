b7923f01e6583cb3cca946f7fb8425b6e71d769f
[Mohamadou Laminou] <[mohamadou055@gmail.com]>
2023-08-03 21:10:56 +0100

    NavigationProcess5

--- JS/main.js
+++ JS/main.js
@@ -94,49 +94,30 @@ var interactionStyle = new ol.style.Style({
 
 // Base map
 
-var bingMapsAerial = new ol.layer.Tile({
-  title: 'Aerial',
-  visible: true,
-  type: 'base',
-  preload: Infinity,
-  source: new ol.source.BingMaps({
-    key: 'AuOKP0N2ww907dY398Ci9ZKg38AqF2jc7q1QchUixWw30TpwdCt4T36ip-OyE49R',
-    imagerySet: 'Aerial',
-  })
-});
-
-var bingMapsAerialWithLabelsOnDemand = new ol.layer.Tile({
-  title: 'Aerial with labels',
-  visible: false,
-  type: 'base',
-  preload: Infinity,
-  source: new ol.source.BingMaps({
-    key: 'AuOKP0N2ww907dY398Ci9ZKg38AqF2jc7q1QchUixWw30TpwdCt4T36ip-OyE49R',
-    imagerySet: 'AerialWithLabelsOnDemand',
-  })
-});
-
-var bingMapsRoadOnDemand = new ol.layer.Tile({
-  title:'Road',
-  visible: false,
-  type: 'base',
-  preload: Infinity,
-  source: new ol.source.BingMaps({
-    key: 'AuOKP0N2ww907dY398Ci9ZKg38AqF2jc7q1QchUixWw30TpwdCt4T36ip-OyE49R',
-    imagerySet: 'RoadOnDemand',
-  })
-});
-
-var bingMapsCanvasDark = new ol.layer.Tile({
-  title: "Canvas Dark",
-  visible: false,
-  type: 'base',
-  preload: Infinity,
-  source: new ol.source.BingMaps({
-    key: 'AuOKP0N2ww907dY398Ci9ZKg38AqF2jc7q1QchUixWw30TpwdCt4T36ip-OyE49R',
-    imagerySet: 'CanvasDark',
-  })
-});
+const styles = [
+  'RoadOnDemand',
+  'Aerial',
+  'AerialWithLabelsOnDemand',
+  'CanvasDark',
+  'OrdnanceSurvey',
+];
+const layers = [];
+let i, ii;
+for (i = 0, ii = styles.length; i < ii; ++i) {
+  layers.push(
+    new ol.layer.Tile({
+      visible: false,
+      preload: Infinity,
+      source: new ol.source.BingMaps({
+        key: 'AuOKP0N2ww907dY398Ci9ZKg38AqF2jc7q1QchUixWw30TpwdCt4T36ip-OyE49R',
+        imagerySet: styles[i],
+        // use maxZoom 19 to see stretched tiles instead of the BingMaps
+        // "no photos at this zoom level" tiles
+        // maxZoom: 19
+      }),
+    })
+  );
+}
 
 
 
@@ -147,23 +128,22 @@ var map = new ol.Map({
     center: ol.proj.fromLonLat([12.744686, 6.434886]),
     zoom: 6,
   }),
-  layers: [bingMapsAerial,bingMapsAerialWithLabelsOnDemand,bingMapsRoadOnDemand,bingMapsCanvasDark],
+  layers: layers,
 });
 
 
-function toggleLayer(eve){
-  var lyrname = eve.target.value;
-  var checkedStatus = eve.target.checked;
-  var lyrList = map.getLayers();
+const select = document.getElementById('layer-select');
+function onChange() {
+  const style = select.value;
+  for (let i = 0, ii = layers.length; i < ii; ++i) {
+    layers[i].setVisible(styles[i] === style);
+  }
 
-  lyrList.forEach(function(element){
-      if (lyrname == element.get('title')){
-          element.setVisible(checkedStatus);
-      }else{
-        element.setVisible(false);
-      }
-  });
 }
+
+
+select.addEventListener('change', onChange);
+onChange();
 
 function bindInputs(layerid, layer) {
   const visibilityInput = $(layerid + ' input.visible');
@@ -219,104 +199,7 @@ var overlayGroup = new ol.layer.Group({
 
 map.addLayer(overlayGroup);
 
-// GeoJSON layer
-var vectorSource = new ol.source.Vector({
- 
-  url: 'RSC/GeoJson/regional.geojson',
-  format: new ol.format.GeoJSON(),
 
-});
-
-
-map.addLayer(new ol.layer.Vector({
-  visible:false,
-  name: 'Regions',
-  source: vectorSource,
-  style: function (f) {
-    return new ol.style.Style({
-      image: new ol.style.RegularShape({
-        radius: 5,
-        radius2: 0,
-        points: 4,
-        stroke: new ol.style.Stroke({ color: "#000", width: 1 })
-      }),
-      text: new ol.style.Text({
-        text: f.get('id').toString(),
-        font: 'bold 11px sans-serif',
-      }),
-      stroke: new ol.style.Stroke({
-        width: 1,
-        color: [255, 128, 0]
-      }),
-      fill: new ol.style.Fill({
-        color: [255, 128, 0, .2]
-      })
-    })
-  }
-}));
-
-// Select  interaction
-var select = new ol.interaction.Select({
-  hitTolerance: 5,
-  multi: true,
-  condition: ol.events.condition.singleClick
-});
-map.addInteraction(select);
-
-// Select control
-var popup = new ol.Overlay.PopupFeature({
-  popupClass: 'default anim',
-  select: select,
-  canFix: true,
-  /** /
-  template: function(f) {
-    return {
-      title: function(f) { return f.get('nom')+' ('+f.get('id')+')' },
-      attributes: { 
-        region: { title: 'Région' }, 
-        arrond: 'arrond', 
-        cantons: 'cantons', 
-        communes: 'communes', 
-        pop: 'pop' 
-      }
-    }
-  },
-  /**/
-  template: {
-    title:
-      // 'nom',   // only display the name
-      function (f) {
-        return f.get('id') + ' (' + f.get('province') + ')';
-      },
-    attributes: // [ 'region', 'arrond', 'cantons', 'communes', 'pop' ]
-    {
-      'id': { title: 'ID_Region' },
-      'province': { title: 'Région' },
-
-      // with prefix and suffix
-      'pop': {
-        title: 'Population',  // attribute's title
-        before: '',           // something to add before
-        format: ol.Overlay.PopupFeature.localString(),  // format as local string
-        after: ' hab.'        // something to add after
-      },
-      // calculated attribute
-      'pop2': {
-        title: 'Population (kHab.)',  // attribute's title
-        format: function (val, f) {
-          return Math.round(parseInt(f.get('pop')) / 100).toLocaleString() + ' kHab.'
-        }
-      }
-      /* Using localString with a date * /
-      'date': { 
-        title: 'Date', 
-        format: ol.Overlay.PopupFeature.localString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }) 
-      }
-      /**/
-    }
-  }
-});
-map.addOverlay(popup);
 
 var sidebar = new ol.control.Sidebar({
   element: 'sidebar',
