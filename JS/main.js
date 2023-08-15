254974bf88a8d1a24e273599eec70fd07c919be6
[Mohamadou Laminou] <[mohamadou055@gmail.com]>
2023-08-15 02:36:58 +0100

    NavigationProcess8

--- JS/main.js
+++ JS/main.js
@@ -92,6 +92,7 @@ var interactionStyle = new ol.style.Style({
     })
 });
 
+
 // Base map (Bing map)
 
 const styles = [
@@ -143,7 +144,6 @@ function onChange() {
 
 }
 
-
 select.addEventListener('change', onChange);
 onChange();
 
@@ -193,17 +193,24 @@ var Communes_CM = new ol.layer.Image({
   })
 });
 
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+     var isIE11 = !!navigator.userAgent.match(/Trident.*rv\:11\./);
+
+         $("#toggleIcon").toggleClass("fa fa-angle-double-down fa fa-angle-double-up")
+         $("#wrapper").toggleClass("toggled");
+ 
+       if(isIE11){
+           if($("#wrapper").hasClass("toggled")){
+         $('#sidebar-wrapper').css("margin-left", "-268px")
+       } else {
+         $('#sidebar-wrapper').css("margin-left", "-250px")	
+           }	 
+     }
+     });
 
 
 
-
-var sidebar = new ol.control.Sidebar({
-  element: 'sidebar',
-  position: 'left'
-});
-
-
-map.addControl(sidebar);
 
 
 // Control
